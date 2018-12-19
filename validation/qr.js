736d0ab8589452f71927a0224e9893dd7d9a94a0
amebrahimi <amirebrahimi5@live.com>
2018-12-19 19:38:28 +0330

    qr validator modified

--- validation/qr.js
+++ validation/qr.js
@@ -5,7 +5,7 @@ module.exports = function validateRegisterInput(data) {
 
     let errors = {};
 
-    if (!Validator.isLength(data.text, {min: 2, max: 30})) {
+    if (!Validator.isLength(data.text, {min: 2, max: undefined})) {
         errors.name = 'text must be between 2 and 30 characters';
     }
 
