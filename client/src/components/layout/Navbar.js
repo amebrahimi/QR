ad12227568286028a810a7fca5d539a34a57309e
amebrahimi <amirebrahimi5@live.com>
2018-12-22 15:01:45 +0330

    modified - navbar mb-5 removed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
-import PropTypes from 'prop-types';
 
 class AppNavbar extends Component {
     render() {
         return (
             <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Navbar color="dark" dark expand="sm">
                     <NavbarBrand href="/">QR Generator</NavbarBrand>
                 </Navbar>
             </div>
         );
     }
 }
-
-AppNavbar.propTypes = {};
 
 export default AppNavbar;
