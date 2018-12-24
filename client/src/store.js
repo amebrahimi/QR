02559aa6cad51045d8cb18bbfc5896e3a773cce0
amebrahimi <amirebrahimi5@live.com>
2018-12-24 13:55:03 +0330

    update - store updated for mobile use

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,19 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import {createLogger} from 'redux-logger';
+
+const logger = createLogger({
+    collapsed: true,
+    diff: true
+});
 
 const initialState = {};
-const middleware = [thunk];
+const middleware = [thunk, logger];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, initialState, composeWithDevTools(
+    applyMiddleware(...middleware)
 ));
 
 export default store;
