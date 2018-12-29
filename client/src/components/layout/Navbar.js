8365f04154338a7b51e09e0a7397d409f9aa2fa2
amebrahimi <amirebrahimi5@live.com>
2018-12-29 16:37:24 +0330

    feature - Navbar updated with statistics

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,40 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
 import {connect} from "react-redux";
 
 class AppNavbar extends Component {
 
+    state = {
+        isOpen: false
+    };
+
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
 
     render() {
-        const {is_navbar_visible} = this.props.nav;
+        const {nav: {is_navbar_visible}, auth: {isAuthenticated}} = this.props;
+        const {isOpen} = this.state;
+
+        const loggedInNavbar = (
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink className="nav-link" href="/dashboard/statistics">Statistics</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        );
 
         const navbar =
-            (<Navbar color="dark" dark expand="sm">
-                <NavbarBrand href="/">QR Generator</NavbarBrand>
+            (<Navbar color="dark" dark expand="md">
+                <NavbarBrand href="/dashboard">QR Generator</NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+                {isAuthenticated && loggedInNavbar}
             </Navbar>);
 
         return (
@@ -22,7 +46,8 @@ class AppNavbar extends Component {
 }
 
 const mapStateToProps = state => ({
-    nav: state.navbar
+    nav: state.navbar,
+    auth: state.auth
 });
 
 export default connect(mapStateToProps)(AppNavbar);
