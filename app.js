d3819a0601514a1b642bcb7c8de40fa3de6a0554
amebrahimi <amirebrahimi5@live.com>
2018-12-30 13:12:50 +0330

    feature - Cors policy updated

--- app.js
+++ app.js
@@ -41,7 +41,13 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static('client/build'));
 
-    app.get('*' , (req, res) => {
+    app.all('*', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    });
+
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
